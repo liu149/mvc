204745742e3e37147b3c1679223ee140c01d9168
liu149 <liuqi1212@outlook.com>
2020-09-16 20:42:52 +0800

    mvc

---
+++ src/main/java/com/lq/AppConfig.java
@@ -0,0 +1,9 @@
+package com.lq;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.lq")
+public class AppConfig {
+}
